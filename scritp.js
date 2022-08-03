98c6cfb180d6bbbd045a5e182b3b162a1ecc35b6
Valderi <valderi-sousa@hotmail.com>
2022-08-03 14:24:58 -0300

    Calculadora-

---
+++ scritp.js
@@ -0,0 +1,23 @@
+// funções declarativa
+/*function funcao(){
+    console.log('Essa mensagem veio de uma funação');
+}
+funcao(); */
+
+/*function mensagem(primeiro, segundo) {
+     console.log(primeiro, segundo) 
+}
+mensagem('tudo certo', 'jovem'); */
+
+// expresssão de função
+/*var funcao = function funcao(){
+    console.log('Sou uma mensagem de função de expressão!')
+}
+funcao();*/
+
+//arrow function
+/*var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+funcao();*/
+
